f94c614a904774fa8a8063b31899142bcbcdba55
Mitsuharu Emoto <mthr1982@gmail.com>
2025-01-07 19:45:49 +0900

    rename

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if [ ! -d "dist" ]; then
 fi
 
 swiftc -Onone ./src/code1.swift -o dist/code1
-swiftc -O ./src/code1.swift -o dist/code1_o
+swiftc -O ./src/code1.swift -o dist/code1_opt
 
 swiftc -Onone ./src/code2.swift -o dist/code2
-swiftc -O ./src/code2.swift -o dist/code2_o
+swiftc -O ./src/code2.swift -o dist/code2_opt
