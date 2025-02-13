d7518a62b539f246c0536d58f0513f3155407582
Mitsuharu Emoto <mthr1982@gmail.com>
2025-02-13 20:52:41 +0900

    計測対象関数の追加

--- build.sh
+++ build.sh
@@ -4,11 +4,17 @@ if [ ! -d "dist" ]; then
   mkdir dist
 fi
 
+swiftc -Onone ./src/code0.swift -o dist/code0
+swiftc -O ./src/code0.swift -o dist/code0_opt
+
 swiftc -Onone ./src/code1.swift -o dist/code1
 swiftc -O ./src/code1.swift -o dist/code1_opt
 
 swiftc -Onone ./src/code2.swift -o dist/code2
 swiftc -O ./src/code2.swift -o dist/code2_opt
+
+swiftc -Onone ./src/code3.swift -o dist/code3
+swiftc -O ./src/code3.swift -o dist/code3_opt
 
 # 計測
 swiftc -Onone ./src/measure/measure.swift  -o dist/measure
---
+++ src/code0.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+for _ in 0..<1_000_000 {
+}
